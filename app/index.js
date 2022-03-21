b1e8baf1c287ad11c770a3b00aeb7f3dd10f7ca5
Jonel Lenteria <jlenteria98@gmail.com>
2022-03-21 11:22:57 +0800

    update uninstall

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 const router = require('./router.js')
 const {app} = require('@adopisoft/exports')
 const services = require('./services.js')
+const config = require('./config.js')
 
 module.exports = {
   async init () {
@@ -9,5 +10,6 @@ module.exports = {
   async uninstall () {
     //delete dataplicity when uninstall this plugin
     await services.deleteDataPlicity()
+    await config.save({enable_dataplicity: false})
   }
 }
